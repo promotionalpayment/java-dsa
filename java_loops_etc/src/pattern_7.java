b67e8095926ac9030a1d75d359e06c61d2fafd03
promotionalpayment <eragonrancho7@gmail.com>
2025-03-12 21:09:09 +0530

    fixing pattern_7

--- java_loops_etc/src/pattern_7.java
+++ java_loops_etc/src/pattern_7.java
@@ -2,9 +2,8 @@ import java.util.Scanner;
 
 public class pattern_7 {
 
-    public static void pattern_7a() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("enter the number of rows(for a half pyramid):");
+    public static void pattern_7a(Scanner sc) {
+        System.out.println("Enter the number of rows (for a half pyramid):");
         int a = sc.nextInt();
 
         for (int i = 1; i <= a; i++) {
@@ -15,28 +14,38 @@ public class pattern_7 {
         }
     }
 
-    public static void pattern_7b()
-            {
-                Scanner sc = new Scanner(System.in);
-                System.out.println("enter the number of rows(for a half pyramid):");
-                int b = sc.nextInt();
+    public static void pattern_7b(Scanner sc) {
+        System.out.println("Enter the number of rows (for an inverted half pyramid):");
+        int b = sc.nextInt();
 
-                for (int i = b; i > 0; i--) {
-                    for (int j = 1; j <= i; j++) {
-                        System.out.print(j);
-                    }
-                    System.out.println();
-
-
-                }
-            }}
-
-            public static void main (String[]args)
-
-            {
-                pattern_7a();
-                pattern_7b();
+        for (int i = b; i > 0; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
             }
+            System.out.println();
         }
+    }
 
+    public static void pattern_7c(int n) {
+        int num = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(num++ + " ");
+            }
+            System.out.println();
+        }
+    }
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        pattern_7a(sc);
+        pattern_7b(sc);
+
+        System.out.println("Enter the number of rows for pattern_7c:");
+        int n = sc.nextInt();
+        pattern_7c(n);
+
+        sc.close();
+    }
+}
